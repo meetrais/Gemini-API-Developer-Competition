c3df8ce34f6ae431e9a3cae503fd9fa94b0e51a7
Rais Kazi <meetrais@gmail.com>
2024-05-19 11:37:18 -0500

    Added code for frontend app.

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from langchain_community.document_loaders import PyPDFLoader
 
 #Load variables from .env file
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
+
+DATA_PATH = 'data/Profile.pdf'
+loader = PyPDFLoader(file_path=DATA_PATH)
+pages = loader.load_and_split()
+pdf_content=''
+for page in pages:
+  pdf_content += page.page_content
     
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
-#response = model.generate_content("What is the meaning of life?")
+prompt="You are a Career Mentor. Provide your guidance based on this profile."
+response = model.generate_content(contents=[prompt,pdf_content])
 
-#print(response.candidates[0].content.parts[0].text)
-
-response = model.generate_content("What is the meaning of life?", stream=True)
-for chunk in response:
-  print(chunk.text)
-  print("_"*80)
+print(response.text)
---
+++ frontend/careermentorfrontendapp/android/app/src/main/kotlin/com/example/careermentorfrontendapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.careermentorfrontendapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
